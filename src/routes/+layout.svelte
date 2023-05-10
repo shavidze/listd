66fc407ff5f388cff6b08c4345f91e64ebd348c8
Saba Shavidze <sabashavidze@gmail.com>
2023-05-10 17:07:40 +0400

    Setup SkeletonUI with tailwind / styled layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,31 @@
 <script>
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// This contains the bulk of Skeletons required styles:
+	// NOTE: this will be renamed skeleton.css in the v2.x release.
+	import '@skeletonlabs/skeleton/styles/all.css';
+
 	import '../app.postcss';
+	import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
-</nav>
-
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+			<svelte:fragment slot="headline">(headline)</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<main class="w-full h-full page-content flex-auto">
+		<slot />
+	</main>
+	<!-- ---- / ---- -->
+	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
+	<!-- (footer) -->
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="pt-2 flex justify-center">
+	<div class="card p-4 max-w-sm w-80 flex flex-col items-center">
+		<p class="p-4">
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+			galley of type and scrambled it to make a type specimen book. It has survived not only five
+			centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It
+			was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+			passages, and more recently with desktop publishing software like Aldus PageMaker including
+			versions of Lorem Ipsum.
+		</p>
+		<button type="button" class="btn variant-filled-primary cursor-pointer inline-block">
+			Sign up
+		</button>
+	</div>
+</div>
