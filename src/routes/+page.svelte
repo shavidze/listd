d73982c8968ff45bf82347938b2253b9ce87ea2e
Saba Shavidze <sabashavidze@gmail.com>
2023-05-10 17:21:21 +0400

    Setup steeze-ui for svelte-icon and tabler-icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="pt-2 flex justify-center">
 	<div class="card p-4 max-w-sm w-80 flex flex-col items-center">
-		<p class="p-4">
+		<p class="my-4">
 			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
 			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
 			galley of type and scrambled it to make a type specimen book. It has survived not only five
